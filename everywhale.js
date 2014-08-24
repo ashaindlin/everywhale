7271d56920068085629bb4b708d96c4905d35350
Alex Shaindlin <ashaindlin@gmail.com>
2014-08-23 23:03:42 -0400

    Use AWS file storage

--- everywhale.js
+++ everywhale.js
@@ -1,25 +1,24 @@
-var fs = require('fs');
 var http = require('http');
 var Twit = require('twit');
+var AWS = require('aws-sdk');
 var config = require('./config.js');
-var T = new Twit(config.twit);
 
-var server = http.createServer(function(request, response) {
+var T = new Twit(config.twit);
+var s3 = new AWS.S3()
+
+http.createServer(function(request, response) {
     response.writeHead(302, {
         'Location': 'http://twitter.com/everywhale'
     });
     response.end();
-});
+}).listen(process.env.PORT);
 
 function tweet() {
-    fs.readFile('./resources/unused.txt', function(err, data) {
+    s3.getObject({ Bucket: 'everywhale', Key: 'unused.txt' }, function(err, data) {
         if (err) throw err;
-        var items = data.toString().split('\n');
+        var items = data.Body.toString().split('\n');
         if (items.length == 0 || (items.length == 1 && items[0] == '')) {
-            T.post('statuses/update', { status: 'completed. i think this project went very whale!' }, function(err, data, res) {
-                console.log("completed!");
-                process.exit(0);
-            });
+            return; // completed!
         }
         var index = Math.floor(Math.random()*items.length);
         var item = items[index];
@@ -29,13 +28,24 @@ function tweet() {
         T.post('statuses/update', { status: message }, function(err, data, res) {
             if (err) throw err;
             console.log(data);
-            fs.writeFileSync('./resources/unused.txt', items.join('\n'));
-            fs.appendFileSync('./resources/used.txt', (item + '\n'));
+            params = { Bucket: 'everywhale', Key: 'unused.txt' };
+            params.Body = items.join('\n');
+            s3.putObject(params, function(err, data) {
+                if (err) throw err;
+                console.log(data);
+            });
+            s3.getObject({ Bucket: 'everywhale', Key: 'used.txt' }, function (err, data) {
+                if (err) throw err;
+                params.Key = 'used.txt';
+                params.Body = data.Body.toString() + item + '\n';
+                s3.putObject(params, function(err, data) {
+                    if (err) throw err;
+                    console.log(data);
+                });
+            });
         });
     });
 }
-
-server.listen(process.env.PORT);
 
 tweet();
 
